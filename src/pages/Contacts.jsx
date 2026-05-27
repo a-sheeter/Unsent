45a80d0ea432ce25c3b0d4c2f7532d21acad7556
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-05-27 14:42:28 -0400

    Contact styling

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../../utilities/supabase";
+
+import Button from "../components/Button";
+
 export default function Contacts() {
     return (
         <div className="main-container">
-            Hello
+            <div className="flex-row space-between">
+                <h1 className="dark-blue-text">Contacts</h1>
+                <Button text="Add New Contact" className="btn secondary-btn"/>
+            </div>
         </div>
     )
 }
