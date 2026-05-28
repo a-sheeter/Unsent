8e3cd05cd7f734efe41a36f0693f41f2e919c92b
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-05-28 14:45:31 -0400

    popup container init

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -7,6 +7,7 @@ import editIcon from "../assets/pen-to-square-solid-full.svg";
 
 import "../styles/table.css";
 import "../styles/contacts.css";
+import PopupContainer from "../components/PopupContainer";
 
 export default function Contacts() {
     /* set meta title */
@@ -31,6 +32,14 @@ export default function Contacts() {
         getContacts();
     }, []);
 
+    /* popup */
+    const [isPopupClosed, setIsPopupClosed] = useState(false);
+
+    function handleClosePopup() {
+        setIsPopupClosed(true);
+    }
+
+    /* get contacts */
     async function getContacts() {
 
         const {
@@ -54,6 +63,7 @@ export default function Contacts() {
 
     }
 
+    /* add contacts */
     async function addContact(e) {
 
         e.preventDefault();
@@ -91,75 +101,81 @@ export default function Contacts() {
     }
 
     return (
-        <div className="main-container">
-            <div className="flex-row space-between">
-                <h1 className="dark-blue-text">Contacts</h1>
-            </div>
+        <>
+            {/* add contacts popup */}
+            <PopupContainer title="Add New Contact" isClosed={isPopupClosed}>
+                <form onSubmit={addContact}>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
 
-            <form onSubmit={addContact}>
-                <input
-                    type="text"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
+                    <input
+                        type="text"
+                        placeholder="Relationship"
+                        value={relationship}
+                        onChange={(e) => setRelationship(e.target.value)}
+                    />
 
-                <input
-                    type="text"
-                    placeholder="Relationship"
-                    value={relationship}
-                    onChange={(e) => setRelationship(e.target.value)}
-                />
+                    <input
+                        type="text"
+                        placeholder="Note"
+                        value={note}
+                        onChange={(e) => setNote(e.target.value)}
+                    />
+                    <Button text="Add New Contact" className="btn secondary-btn" type="submit" oncClick={handleClosePopup}/>
+                </form>
+            </PopupContainer>
 
-                <input
-                    type="text"
-                    placeholder="Note"
-                    value={note}
-                    onChange={(e) => setNote(e.target.value)}
-                />
-                <Button text="Add New Contact" className="btn secondary-btn" type="submit" />
-            </form>
+            {/* Main container */}
+            <div className="main-container">
+                <div className="flex-row space-between">
+                    <h1 className="dark-blue-text">Contacts</h1>
+                </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Relationship</th>
-                        <th>Last written</th>
-                        <th>Optional Note</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {contacts.map((contact, index) => {
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Relationship</th>
+                            <th>Last written</th>
+                            <th>Optional Note</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contacts.map((contact, index) => {
 
-                        const initials = contact.name
-                            .split(" ")
-                            .map(word => word[0])
-                            .join("")
-                            .toUpperCase();
+                            const initials = contact.name
+                                .split(" ")
+                                .map(word => word[0])
+                                .join("")
+                                .toUpperCase();
 
-                        const avatarClass = avatarColors[index % avatarColors.length];
+                            const avatarClass = avatarColors[index % avatarColors.length];
 
-                        return (
-                            <tr key={contact.id}>
-                                <td>
-                                    <div className="contact-name-wrapper">
-                                        <div className={`avatar-circle ${avatarClass}`}>
-                                            {initials}
+                            return (
+                                <tr key={contact.id}>
+                                    <td>
+                                        <div className="contact-name-wrapper">
+                                            <div className={`avatar-circle ${avatarClass}`}>
+                                                {initials}
+                                            </div>
+                                            <span>{contact.name}</span>
                                         </div>
-                                        <span>{contact.name}</span>
-                                    </div>
                                     </td>
-                                <td>{contact.relationship}</td>
-                                <td>{contact.last_written}</td>
-                                <td>{contact.note}</td>
-                                <td><button><svg width="26" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="rgb(27, 45, 72)" d="M535.6 85.7C513.7 63.8 478.3 63.8 456.4 85.7L432 110.1L529.9 208L554.3 183.6C576.2 161.7 576.2 126.3 554.3 104.4L535.6 85.7zM236.4 305.7C230.3 311.8 225.6 319.3 222.9 327.6L193.3 416.4C190.4 425 192.7 434.5 199.1 441C205.5 447.5 215 449.7 223.7 446.8L312.5 417.2C320.7 414.5 328.2 409.8 334.4 403.7L496 241.9L398.1 144L236.4 305.7zM160 128C107 128 64 171 64 224L64 480C64 533 107 576 160 576L416 576C469 576 512 533 512 480L512 384C512 366.3 497.7 352 480 352C462.3 352 448 366.3 448 384L448 480C448 497.7 433.7 512 416 512L160 512C142.3 512 128 497.7 128 480L128 224C128 206.3 142.3 192 160 192L256 192C273.7 192 288 177.7 288 160C288 142.3 273.7 128 256 128L160 128z"/></svg></button></td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
+                                    <td>{contact.relationship}</td>
+                                    <td>{contact.last_written}</td>
+                                    <td>{contact.note}</td>
+                                    <td><button><svg width="26" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="rgb(27, 45, 72)" d="M535.6 85.7C513.7 63.8 478.3 63.8 456.4 85.7L432 110.1L529.9 208L554.3 183.6C576.2 161.7 576.2 126.3 554.3 104.4L535.6 85.7zM236.4 305.7C230.3 311.8 225.6 319.3 222.9 327.6L193.3 416.4C190.4 425 192.7 434.5 199.1 441C205.5 447.5 215 449.7 223.7 446.8L312.5 417.2C320.7 414.5 328.2 409.8 334.4 403.7L496 241.9L398.1 144L236.4 305.7zM160 128C107 128 64 171 64 224L64 480C64 533 107 576 160 576L416 576C469 576 512 533 512 480L512 384C512 366.3 497.7 352 480 352C462.3 352 448 366.3 448 384L448 480C448 497.7 433.7 512 416 512L160 512C142.3 512 128 497.7 128 480L128 224C128 206.3 142.3 192 160 192L256 192C273.7 192 288 177.7 288 160C288 142.3 273.7 128 256 128L160 128z" /></svg></button></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
