783b08783769e0cd0b3ed1f1510ad3789725a609
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-06-05 15:52:26 -0400

    Code cleanup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,48 +2,50 @@ import { useState, useEffect } from "react";
 import { supabase } from "../../utilities/supabase";
 
 export default function Profile() {
-    /* set meta title */
+
+    /* --- State --- */
+    const [profile, setProfile] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    /* --- Effects ---*/
     useEffect(() => {
         document.title = "Profile";
     }, []);
 
-    const [profile, setProfile] = useState(null);
-    const [loading, setLoading] = useState(true);
-
     useEffect(() => {
+        loadProfile();
+    }, []);
 
-        async function loadProfile() {
-            setLoading(true);
+    async function loadProfile() {
+        setLoading(true);
 
-            const { data: userData, error: userError } = await supabase.auth.getUser();
+        const { data: userData, error: userError } = await supabase.auth.getUser();
 
-            const user = userData?.user;
+        const user = userData?.user;
 
-            if (userError || !user) {
-                console.log("No user found");
-                setLoading(false);
-                return;
-            }
-
-            const { data, error } = await supabase
-                .from("profiles")
-                .select("name, username")
-                .eq("id", user.id)
-                .single();
-
-            if (error) {
-                console.log(error);
-                setLoading(false);
-                return;
-            }
-
-            setProfile(data);
+        if (userError || !user) {
+            setErrorMessage("Unable to load profile.");
             setLoading(false);
+            return;
         }
 
-        loadProfile();
+        const { data, error } = await supabase
+            .from("profiles")
+            .select("name, username")
+            .eq("id", user.id)
+            .single();
 
-    }, []);
+        if (error) {
+            setErrorMessage("Unable to load profile.")
+            setLoading(false);
+            return;
+        }
+
+        setProfile(data);
+        setLoading(false);
+    }
 
     if (loading) {
         return (
@@ -54,6 +56,7 @@ export default function Profile() {
         );
     }
 
+    /* --- Render --- */
     return (
         <div className="main-container">
             <h1 className="dark-blue-text">User Profile</h1>
@@ -66,6 +69,10 @@ export default function Profile() {
 
             <p className="gray-text">Password</p>
             <p>*********</p>
+
+            {errorMessage && (
+                <p className="form-error">{errorMessage}</p>
+            )}
         </div>
     );
 }
